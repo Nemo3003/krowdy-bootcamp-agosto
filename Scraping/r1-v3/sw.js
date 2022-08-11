92251ea807ee313d30e0dfe6d1bfdb308cdfc843
Nemo3003 <ulimarian2000@gmail.com>
2022-08-11 15:40:53 -0300

    [UPD] Yet another version of a new one

--- Scraping/r1-v3/sw.js
+++ Scraping/r1-v3/sw.js
@@ -1,25 +1,28 @@
 console.log("Its alive!!  you monster!....");
-let yooza = 'nel'
+
+import {postData} from './profilesInject.js';
+
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
-        function: (/^https:\/\/www\.linkedin\.com\/.+/).test(tab.url)? yooza:()=> alert("Yooza!You ain't supposed to scrap here bro.")
+        function: (/^https:\/\/www\.linkedin\.com\/.+/).test(tab.url)? postData:()=> alert("Yooza!You ain't supposed to scrap here bro.")
     })
 })
 
 chrome.runtime.onMessage.addListener((message)=> {
     console.log(message);
-    chrome.notifications.create({title: "AKA: stalking", message: `You've done your stalking: Charge${message.id}`, iconUrl:message.iconUrl, type: "basic"});
+    chrome.notifications.create({
+        title: "AKA: stalking", 
+        message: `You've done your stalking: Charge${message.id}`, iconUrl:message.iconUrl, type: "basic"});
 
-    //sending data with fetch native method
-    var url = 'https://service.com/saveProfile';
+    //mock sending data with fetch method
+    var url = 'https://example.com/saveProfile';
     var data = {idProfile: message.id, profileImg: message.iconUrl};
-
     fetch(url, {
     method: 'POST',
     body: JSON.stringify(data),
     headers:{
-        'Content-Type': 'no idea'
+        'Content-Type': 'application/json'
     }
     }).then(res => res.json())
     .catch(error => console.error('Error:', error))
