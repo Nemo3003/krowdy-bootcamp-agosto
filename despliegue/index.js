3058b82d73e1c7dfa417b73afcc526bd03542772
Nemo3003 <ulimarian2000@gmail.com>
2022-09-03 19:34:49 -0300

    [UPD] Dockerfile added. More funcs

--- despliegue/index.js
+++ despliegue/index.js
@@ -1,22 +1,28 @@
-const { application, response } = require('express');
-const express = require('express');
-const app = express();
-let mysql = require('mysql');
-
-const testing = [
-    {
-        "id": 1,
-        "name": "test name"
-    }
-]
-
-  app.get('/', (req, res)=>{
-
-    res.json(testing)
-})
-
+const express = require('express')
+const mongoose = require('mongoose')
 
 const PORT = 8085
-app.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`)
-} )
+const HOST = 'localhost'
+const app = express()
+mongoose.connect('mongodb://localhost:27017')
+
+const Client = mongoose.model('Client', new mongoose.Schema({
+    name: String
+}));
+
+app.get('/', (_req, res) => {
+    res.send('Welcome! Add your new client: ')
+    let client= Client.find()
+    return res.send(client)
+})
+
+app.get('/register', (req, res) => {
+    let name = req.query.name
+    console.log('Client being created')
+    Client.create({ name: name="Ulises" })    
+    console.log('Client created successfully')
+    res.send(`Client Registered: ${name}`)
+})
+
+app.listen(PORT, HOST)
+console.log(`running on http://${HOST}:${PORT}`)
